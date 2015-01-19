427ecf29ae77e8b47374f9b37eebcca823419f87
ScrappingFTW <ScrappingFTW@users.noreply.github.com>
2015-01-19 14:31:53 +0100

    Extract Settings super class for better code reuse and also for providing a common storage location for all settings subclasses

--- JAVMovieScraper/src/moviescraper/doctord/preferences/MoviescraperPreferences.java
+++ JAVMovieScraper/src/moviescraper/doctord/preferences/MoviescraperPreferences.java
@@ -3,36 +3,32 @@ package moviescraper.doctord.preferences;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 import org.apache.commons.lang3.SystemUtils;
 
-public class MoviescraperPreferences {
+public class MoviescraperPreferences extends Settings {
 
-	Properties programPreferences;
-	private static final String fileNameOfPreferences = "settings.xml";
 	//for the property names, it's important (because of reflection) that the values be kept the same as the variable names
 	//this lets me be lazy and reuse code for each property setter/getter using a common method :)
 	//so don't go renaming these variables without being aware of the consequences!
-	private static final String lastUsedDirectory = "lastUsedDirectory";
-	private static final String writeFanartAndPosters = "writeFanartAndPosters";
-	private static final String overwriteFanartAndPosters = "overwriteFanartAndPosters";
-	private static final String downloadActorImagesToActorFolder = "downloadActorImagesToActorFolder";
-	private static final String extraFanartScrapingEnabled = "extraFanartScrapingEnabled";
-	private static final String createFolderJpg = "createFolderJpg";
-	private static final String noMovieNameInImageFiles = "noMovieNameInImageFiles";
-	private static final String writeTrailerToFile = "writeTrailerToFile";
-	private static final String nfoNamedMovieDotNfo = "nfoNamedMovieDotNfo";
-	private static final String useIAFDForActors = "useIAFDForActors";
-	private static final String sanitizerForFilename = "sanitizerForFilename";
-	private static final String renamerString = "renamerString";
-	private static final String renameMovieFile = "renameMovieFile";
-	private static final String scrapeInJapanese = "scrapeInJapanese";
-	private static final String promptForUserProvidedURLWhenScraping = "promptForUserProvidedURLWhenScraping";
+	protected static final String lastUsedDirectory = "lastUsedDirectory";
+	protected static final String writeFanartAndPosters = "writeFanartAndPosters";
+	protected static final String overwriteFanartAndPosters = "overwriteFanartAndPosters";
+	protected static final String downloadActorImagesToActorFolder = "downloadActorImagesToActorFolder";
+	protected static final String extraFanartScrapingEnabled = "extraFanartScrapingEnabled";
+	protected static final String createFolderJpg = "createFolderJpg";
+	protected static final String noMovieNameInImageFiles = "noMovieNameInImageFiles";
+	protected static final String writeTrailerToFile = "writeTrailerToFile";
+	protected static final String nfoNamedMovieDotNfo = "nfoNamedMovieDotNfo";
+	protected static final String useIAFDForActors = "useIAFDForActors";
+	protected static final String sanitizerForFilename = "sanitizerForFilename";
+	protected static final String renamerString = "renamerString";
+	protected static final String renameMovieFile = "renameMovieFile";
+	protected static final String scrapeInJapanese = "scrapeInJapanese";
+	protected static final String promptForUserProvidedURLWhenScraping = "promptForUserProvidedURLWhenScraping";
 	
 	/*
 	 * useContentBasedTypeIcons:
@@ -42,40 +38,14 @@ public class MoviescraperPreferences {
 	 * this option is also automatically enabled on linux
 	 */
 	
-	private static final String useContentBasedTypeIcons = "useContentBasedTypeIcons";
+	protected static final String useContentBasedTypeIcons = "useContentBasedTypeIcons";
 
-	public MoviescraperPreferences()
-	{
-		programPreferences = new Properties();
-		try {
-			programPreferences.loadFromXML(new FileInputStream(fileNameOfPreferences));
-			//initialize default values that must exist in the settings file
-			setSanitizerForFilename(getSanitizerForFilename());
-			setRenamerString(getRenamerString());
-		} catch (InvalidPropertiesFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			savePreferences(); //file doesn't exist. this will create the file
-			//initialize default values that must exist in the settings file
-			setSanitizerForFilename(getSanitizerForFilename());
-			setRenamerString(getRenamerString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public void savePreferences(){
-		try {
-			programPreferences.storeToXML(new FileOutputStream(fileNameOfPreferences), "");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public MoviescraperPreferences(){
+		super();
+		
+		//initialize default values that must exist in the settings file
+		setSanitizerForFilename(getSanitizerForFilename());
+		setRenamerString(getRenamerString());
 	}
 
 	public File getLastUsedDirectory(){
@@ -108,48 +78,6 @@ public class MoviescraperPreferences {
 		setBooleanValue(writeFanartAndPosters, preferenceValue);
 	}
 
-
-
-	private void setBooleanValue(String preferenceName, boolean preferenceValue) {
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			if(preferenceValue)
-				programPreferences.setProperty((String)fieldToUse.get(new String()), "true");
-			else
-				programPreferences.setProperty((String)fieldToUse.get(new String()), "false");
-			savePreferences();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-	/**
-	 * 
-	 * @param preferenceName the preference field to set - must be the exact same as the field's local variable name in {@link MoviescraperPreferences}
-	 * @param defaultValue the value to return if the preference has not been set
-	 * @return
-	 */
-	private boolean getBooleanValue(String preferenceName, boolean defaultValue)
-	{
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			String fieldValue = (String)fieldToUse.get(new String());
-			String preferenceValue = programPreferences.getProperty(fieldValue);
-			if(preferenceValue == null)
-				return defaultValue;
-			if(preferenceValue.equals("true"))
-				return true;
-			else if(preferenceValue.equals("false"))
-				return false;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return defaultValue;
-	}
 
 
 	public void setDownloadActorImagesToActorFolderPreference(boolean preferenceValue)
@@ -276,9 +204,6 @@ public class MoviescraperPreferences {
 	public void setPromptForUserProvidedURLWhenScraping(boolean preferenceValue){
 		setBooleanValue(promptForUserProvidedURLWhenScraping, preferenceValue);
 	}
-	
-	public String toString(){
-		return programPreferences.toString();
-	}
+
 
 }
---
+++ JAVMovieScraper/src/moviescraper/doctord/preferences/Settings.java
@@ -0,0 +1,93 @@
+package moviescraper.doctord.preferences;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
+public class Settings {
+
+	protected static Properties programPreferences;
+	protected static final String fileNameOfPreferences = "settings.xml";
+	
+	/*initialization block*/{
+		programPreferences = new Properties();
+		try {
+			programPreferences.loadFromXML(new FileInputStream(fileNameOfPreferences));
+		} catch (InvalidPropertiesFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			savePreferences(); //file doesn't exist. this will create the file
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Settings() {
+		super();
+	}
+
+	public void savePreferences() {
+		try {
+			programPreferences.storeToXML(new FileOutputStream(fileNameOfPreferences), "");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void setBooleanValue(String preferenceName, boolean preferenceValue) {
+		Field fieldToUse;
+		try {
+			fieldToUse = this.getClass().getDeclaredField(preferenceName);
+			if(preferenceValue)
+				programPreferences.setProperty((String)fieldToUse.get(new String()), "true");
+			else
+				programPreferences.setProperty((String)fieldToUse.get(new String()), "false");
+			savePreferences();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+
+	/**
+	 * 
+	 * @param preferenceName the preference field to set - must be the exact same as the field's local variable name in {@link MoviescraperPreferences}
+	 * @param defaultValue the value to return if the preference has not been set
+	 * @return
+	 */
+	protected boolean getBooleanValue(String preferenceName, boolean defaultValue) {
+		Field fieldToUse;
+		try {
+			fieldToUse = this.getClass().getDeclaredField(preferenceName);
+			String fieldValue = (String)fieldToUse.get(new String());
+			String preferenceValue = programPreferences.getProperty(fieldValue);
+			if(preferenceValue == null)
+				return defaultValue;
+			if(preferenceValue.equals("true"))
+				return true;
+			else if(preferenceValue.equals("false"))
+				return false;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return defaultValue;
+	}
+	
+	
+	public String toString(){
+		return programPreferences.toString();
+	}
+
+}
