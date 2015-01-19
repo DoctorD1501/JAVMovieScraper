36aa4997ef28a812c93340f87712a17ceaea3e82
ScrappingFTW <ScrappingFTW@users.noreply.github.com>
2015-01-19 17:27:14 +0100

    Refactor string setting keys into enums

--- JAVMovieScraper/src/moviescraper/doctord/preferences/MoviescraperPreferences.java
+++ JAVMovieScraper/src/moviescraper/doctord/preferences/MoviescraperPreferences.java
@@ -1,45 +1,36 @@
 package moviescraper.doctord.preferences;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.InvalidPropertiesFormatException;
-import java.util.Properties;
 
 import org.apache.commons.lang3.SystemUtils;
 
 public class MoviescraperPreferences extends Settings {
 
-	//for the property names, it's important (because of reflection) that the values be kept the same as the variable names
-	//this lets me be lazy and reuse code for each property setter/getter using a common method :)
-	//so don't go renaming these variables without being aware of the consequences!
-	protected static final String lastUsedDirectory = "lastUsedDirectory";
-	protected static final String writeFanartAndPosters = "writeFanartAndPosters";
-	protected static final String overwriteFanartAndPosters = "overwriteFanartAndPosters";
-	protected static final String downloadActorImagesToActorFolder = "downloadActorImagesToActorFolder";
-	protected static final String extraFanartScrapingEnabled = "extraFanartScrapingEnabled";
-	protected static final String createFolderJpg = "createFolderJpg";
-	protected static final String noMovieNameInImageFiles = "noMovieNameInImageFiles";
-	protected static final String writeTrailerToFile = "writeTrailerToFile";
-	protected static final String nfoNamedMovieDotNfo = "nfoNamedMovieDotNfo";
-	protected static final String useIAFDForActors = "useIAFDForActors";
-	protected static final String sanitizerForFilename = "sanitizerForFilename";
-	protected static final String renamerString = "renamerString";
-	protected static final String renameMovieFile = "renameMovieFile";
-	protected static final String scrapeInJapanese = "scrapeInJapanese";
-	protected static final String promptForUserProvidedURLWhenScraping = "promptForUserProvidedURLWhenScraping";
-	
-	/*
-	 * useContentBasedTypeIcons:
-	 * Use icons in res/mime instead of system icons. 
-	 * Needed for linux as system icons only show two types of icons otherwise (files and folders)
-	 * There's no menu option for this preference, but you can manually modify the settings file yourself to enable it
-	 * this option is also automatically enabled on linux
-	 */
-	
-	protected static final String useContentBasedTypeIcons = "useContentBasedTypeIcons";
+	enum Key implements Settings.Key {
+		lastUsedDirectory,
+		writeFanartAndPosters,
+		overwriteFanartAndPosters,
+		downloadActorImagesToActorFolder,
+		extraFanartScrapingEnabled,
+		createFolderJpg,
+		noMovieNameInImageFiles,
+		writeTrailerToFile,
+		nfoNamedMovieDotNfo,
+		useIAFDForActors,
+		sanitizerForFilename, 
+		renamerString,
+		renameMovieFile,
+		scrapeInJapanese,
+		promptForUserProvidedURLWhenScraping,
+		useContentBasedTypeIcons,
+		;
 
+		@Override
+		public String getKey() {
+			return toString();
+		}
+	}
+	
 	public MoviescraperPreferences(){
 		super();
 		
@@ -49,7 +40,8 @@ public class MoviescraperPreferences extends Settings {
 	}
 
 	public File getLastUsedDirectory(){
-		String lastUsedDir = getStringValue(lastUsedDirectory, null);
+		String lastUsedDir =  getStringValue(Key.lastUsedDirectory, null);
+
 		if(lastUsedDir != null)
 		{
 			File lastUsedDirFile = new File(lastUsedDir);
@@ -61,145 +53,143 @@ public class MoviescraperPreferences extends Settings {
 	}
 
 	public void setLastUsedDirectory(File lastUsedDirectoryFile){
-		setStringValue(lastUsedDirectory, lastUsedDirectoryFile.getPath());
+		setStringValue(Key.lastUsedDirectory, lastUsedDirectoryFile.getPath());
 	}
 
 	public void setOverWriteFanartAndPostersPreference(boolean preferenceValue){
-		setBooleanValue(overwriteFanartAndPosters, preferenceValue);
+		setBooleanValue(Key.overwriteFanartAndPosters, preferenceValue);
 	}
 
 	public boolean getOverWriteFanartAndPostersPreference()
 	{
-		return getBooleanValue(overwriteFanartAndPosters, true);
+		return getBooleanValue(Key.overwriteFanartAndPosters, true);
 	}
 
 	public void setWriteFanartAndPostersPreference(boolean preferenceValue){
-		setBooleanValue(writeFanartAndPosters, preferenceValue);
+		setBooleanValue(Key.writeFanartAndPosters, preferenceValue);
 	}
 
 
 
 	public void setDownloadActorImagesToActorFolderPreference(boolean preferenceValue)
 	{
-		setBooleanValue(downloadActorImagesToActorFolder, preferenceValue);
+		setBooleanValue(Key.downloadActorImagesToActorFolder, preferenceValue);
 	}
 
 	public boolean getDownloadActorImagesToActorFolderPreference()
 	{
-		return getBooleanValue(downloadActorImagesToActorFolder, true);
+		return getBooleanValue(Key.downloadActorImagesToActorFolder, true);
 	}
 
 	public boolean getWriteFanartAndPostersPreference() {
-		/*String writeFanartAndPostersPref = getStringValue(writeFanartAndPosters);
-		if(writeFanartAndPostersPref != null)
-		{
-		if(writeFanartAndPostersPref.equals("true"))
-			return true;
-		else return false;
-		}
-		else return true; //default value if no preference has been set yet*/
-		return getBooleanValue(writeFanartAndPosters, true);
+		return getBooleanValue(Key.writeFanartAndPosters, true);
 	}
 
 	public boolean getExtraFanartScrapingEnabledPreference() {
-		return getBooleanValue(extraFanartScrapingEnabled, false);
+		return getBooleanValue(Key.extraFanartScrapingEnabled, false);
 	}
 
 	public void setExtraFanartScrapingEnabledPreference(boolean preferenceValue){
-		setBooleanValue(extraFanartScrapingEnabled, preferenceValue);
+		setBooleanValue(Key.extraFanartScrapingEnabled, preferenceValue);
 	}
 
 	public void setCreateFolderJpgEnabledPreference(boolean preferenceValue) {
-		setBooleanValue(createFolderJpg, preferenceValue);
+		setBooleanValue(Key.createFolderJpg, preferenceValue);
 
 	}
 
 	public boolean getCreateFolderJpgEnabledPreference() {
-		return getBooleanValue(createFolderJpg, false);
+		return getBooleanValue(Key.createFolderJpg, false);
 	}
 
 	public boolean getNoMovieNameInImageFiles(){
-		return getBooleanValue(noMovieNameInImageFiles, false);
+		return getBooleanValue(Key.noMovieNameInImageFiles, false);
 	}
 
 	public void setNoMovieNameInImageFiles(boolean preferenceValue){
-		setBooleanValue(noMovieNameInImageFiles, preferenceValue);
+		setBooleanValue(Key.noMovieNameInImageFiles, preferenceValue);
 	}
 
 	public boolean getWriteTrailerToFile(){
-		return getBooleanValue(writeTrailerToFile, false);
+		return getBooleanValue(Key.writeTrailerToFile, false);
 	}
 
 	public void setWriteTrailerToFile(boolean preferenceValue){
-		setBooleanValue(writeTrailerToFile, preferenceValue);
+		setBooleanValue(Key.writeTrailerToFile, preferenceValue);
 	}
 
 	public boolean getNfoNamedMovieDotNfo(){
-		return getBooleanValue(nfoNamedMovieDotNfo, false);
+		return getBooleanValue(Key.nfoNamedMovieDotNfo, false);
 	}
 
 	public void setNfoNamedMovieDotNfo(boolean preferenceValue){
-		setBooleanValue(nfoNamedMovieDotNfo, preferenceValue);
+		setBooleanValue(Key.nfoNamedMovieDotNfo, preferenceValue);
 	}
 
 	public boolean getUseIAFDForActors() {
-		return getBooleanValue(useIAFDForActors, false);
+		return getBooleanValue(Key.useIAFDForActors, false);
 	}
 
 	public void setUseIAFDForActors(boolean preferenceValue) {
-		setBooleanValue(useIAFDForActors, preferenceValue);
+		setBooleanValue(Key.useIAFDForActors, preferenceValue);
 	}
 
 	public String getSanitizerForFilename() {
-		return getStringValue(sanitizerForFilename, "[\\\\/:*?\"<>|\\r\\n]|[ ]+$|(?<=[^.])[.]+$|(?<=.{250})(.+)(?=[.]\\p{Alnum}{3}$)");
+		return getStringValue(Key.sanitizerForFilename, "[\\\\/:*?\"<>|\\r\\n]|[ ]+$|(?<=[^.])[.]+$|(?<=.{250})(.+)(?=[.]\\p{Alnum}{3}$)");
 	}
 
 	public void setSanitizerForFilename(String preferenceValue) {
-		setStringValue(sanitizerForFilename, preferenceValue);
+		setStringValue(Key.sanitizerForFilename, preferenceValue);
 	}
 
 	public String getRenamerString() {
-		return getStringValue(renamerString, "<TITLE> [<ACTORS>] (<YEAR>) [<ID>]");
+		return getStringValue(Key.renamerString, "<TITLE> [<ACTORS>] (<YEAR>) [<ID>]");
 	}
 
 	public void setRenamerString(String preferenceValue) {
-		setStringValue(renamerString, preferenceValue);
+		setStringValue(Key.renamerString, preferenceValue);
 	}
 
 	public boolean getRenameMovieFile() {
-		return getBooleanValue(renameMovieFile, false);
+		return getBooleanValue(Key.renameMovieFile, false);
 	}
 
 	public void setRenameMovieFile(boolean preferenceValue) {
-		setBooleanValue(renameMovieFile, preferenceValue);
+		setBooleanValue(Key.renameMovieFile, preferenceValue);
 	}
 
 	public boolean getScrapeInJapanese(){
-		return getBooleanValue(scrapeInJapanese, false);
+		return getBooleanValue(Key.scrapeInJapanese, false);
 	}
 
 	public void setScrapeInJapanese(boolean preferenceValue){
-		setBooleanValue(scrapeInJapanese, preferenceValue);
+		setBooleanValue(Key.scrapeInJapanese, preferenceValue);
 	}
 
 	public boolean getUseContentBasedTypeIcons() {
+	/*    
+     * Use icons in res/mime instead of system icons. 
+     * Needed for linux as system icons only show two types of icons otherwise (files and folders)
+     * There's no menu option for this preference, but you can manually modify the settings file yourself to enable it
+     * this option is also automatically enabled on linux
+     */
 
 		// if we're on linux we want the content based icons as default        
 		boolean defaultValue = SystemUtils.IS_OS_LINUX;
 
-		return getBooleanValue(useContentBasedTypeIcons, defaultValue);
+		return getBooleanValue(Key.useContentBasedTypeIcons, defaultValue);
 	}
 
 	public void setUseContentBasedTypeIcons(boolean preferenceValue) {
-		setBooleanValue(useContentBasedTypeIcons, preferenceValue);    
+		setBooleanValue(Key.useContentBasedTypeIcons, preferenceValue);    
 	}
 	
 	public boolean getPromptForUserProvidedURLWhenScraping(){
-		return getBooleanValue(promptForUserProvidedURLWhenScraping, false);
+		return getBooleanValue(Key.promptForUserProvidedURLWhenScraping, false);
 	}
 
 	public void setPromptForUserProvidedURLWhenScraping(boolean preferenceValue){
-		setBooleanValue(promptForUserProvidedURLWhenScraping, preferenceValue);
+		setBooleanValue(Key.promptForUserProvidedURLWhenScraping, preferenceValue);
 	}
 
 
--- JAVMovieScraper/src/moviescraper/doctord/preferences/Settings.java
+++ JAVMovieScraper/src/moviescraper/doctord/preferences/Settings.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
@@ -12,7 +11,11 @@ public class Settings {
 
 	private static Properties programPreferences;
 	private static final String fileNameOfPreferences = "settings.xml";
-	
+
+	protected interface Key {
+		String getKey(); 
+	}
+
 	/*initialization block*/{
 		programPreferences = new Properties();
 		try {
@@ -44,84 +47,50 @@ public class Settings {
 		}
 	}
 
-	protected void setBooleanValue(String preferenceName, boolean preferenceValue) {
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			if(preferenceValue)
-				programPreferences.setProperty((String)fieldToUse.get(new String()), "true");
-			else
-				programPreferences.setProperty((String)fieldToUse.get(new String()), "false");
-			savePreferences();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
+	protected <K extends Key> void setBooleanValue(K preferenceName, boolean preferenceValue) {
+		String key = preferenceName.getKey();
+		if(preferenceValue)
+			programPreferences.setProperty(key, "true");
+		else
+			programPreferences.setProperty(preferenceName.toString(), "false");
+		
+		savePreferences();
 	}
 
 	/**
 	 * 
-	 * @param preferenceName the preference field to set - must be the exact same as the field's local variable name in {@link MoviescraperPreferences}
+	 * @param preferenceName the preference field to set
 	 * @param defaultValue the value to return if the preference has not been set
 	 * @return
 	 */
-	protected boolean getBooleanValue(String preferenceName, boolean defaultValue) {
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			String fieldValue = (String)fieldToUse.get(new String());
-			String preferenceValue = programPreferences.getProperty(fieldValue);
-			if(preferenceValue == null)
-				return defaultValue;
-			if(preferenceValue.equals("true"))
-				return true;
-			else if(preferenceValue.equals("false"))
-				return false;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	protected <K extends Key> boolean getBooleanValue(K preferenceName, boolean defaultValue) {
+		String fieldValue = preferenceName.getKey();
+		String preferenceValue = programPreferences.getProperty(fieldValue);
+		if(preferenceValue == null)
+			return defaultValue;
+		if(preferenceValue.equals("true"))
+			return true;
+		else if(preferenceValue.equals("false"))
+			return false;
+		
 		return defaultValue;
 	}
 	
-
-	protected void setStringValue(String preferenceName, String preferenceValue) {
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			programPreferences.setProperty((String)fieldToUse.get(new String()), preferenceValue);
-			savePreferences();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
+	protected <K extends Key>  void setStringValue(K preferenceName, String preferenceValue) {
+		programPreferences.setProperty(preferenceName.getKey(), preferenceValue);
+		savePreferences();
 	}
 
-	/**
-	 * 
-	 * @param preferenceName the preference field to set - must be the exact same as the field's local variable name in {@link MoviescraperPreferences}
-	 * @param defaultValue the value to return if the preference has not been set
-	 * @return
-	 */
-	protected String getStringValue(String preferenceName, String defaultValue) {
-		Field fieldToUse;
-		try {
-			fieldToUse = this.getClass().getDeclaredField(preferenceName);
-			String fieldValue = (String)fieldToUse.get(new String());
-			String preferenceValue = programPreferences.getProperty(fieldValue);
-			if(preferenceValue != null)
-				return preferenceValue;
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	protected <K extends Key> String getStringValue(K preferenceName, String defaultValue) {
+		String fieldValue = preferenceName.getKey();
+		String preferenceValue = programPreferences.getProperty(fieldValue);
+		if(preferenceValue != null)
+			return preferenceValue;
+		
 		return defaultValue;
 	}
 	
-	
+		
 	public String toString(){
 		return programPreferences.toString();
 	}
