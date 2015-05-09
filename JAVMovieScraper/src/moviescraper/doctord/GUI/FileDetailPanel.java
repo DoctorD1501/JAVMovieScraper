e1a8e9e99e3c738919292a22fd55cd08160e69c7
DoctorD1501 <DoctorD1501@gmail.com>
2015-05-09 09:42:28 -0700

    Double click genre or actor to edit that item.

--- JAVMovieScraper/src/moviescraper/doctord/GUI/FileDetailPanel.java
+++ JAVMovieScraper/src/moviescraper/doctord/GUI/FileDetailPanel.java
@@ -1,7 +1,6 @@
 package moviescraper.doctord.GUI;
 
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
