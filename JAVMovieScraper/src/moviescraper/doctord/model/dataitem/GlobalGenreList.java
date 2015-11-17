08858b1c7e28254f69b2760e177900e6dba4f943
MadAustrian <madaustrian@users.noreply.github.com>
2015-11-17 07:54:59 +0100

    Genrelist
    XML file & checkboxes

---
+++ JAVMovieScraper/src/moviescraper/doctord/model/dataitem/GlobalGenreList.java
@@ -0,0 +1,37 @@
+package moviescraper.doctord.model.dataitem;
+
+
+// madaustrian
+// whole file to be added
+
+	import javax.swing.JCheckBox;
+
+	
+
+public class GlobalGenreList{
+		
+	public static JCheckBox[] GenreButton_1;
+	public static JCheckBox[] GenreButton_2;
+	public static JCheckBox[] GenreButton_3;
+	public static JCheckBox[] GenreButton_4;
+	public static JCheckBox[] GenreButton_5;
+		
+	public static String GenreList_1[];
+	public static String GenreList_2[];
+	public static String GenreList_3[];
+	public static String GenreList_4[];
+	public static String GenreList_5[];
+		
+	public static int NumberOfGenreItems_1;
+	public static int NumberOfGenreItems_2;
+	public static int NumberOfGenreItems_3;
+	public static int NumberOfGenreItems_4;
+	public static int NumberOfGenreItems_5;
+
+	public static int RealNumberOfGenreItems_1;
+	public static int RealNumberOfGenreItems_2;
+	public static int RealNumberOfGenreItems_3;
+	public static int RealNumberOfGenreItems_4;
+	public static int RealNumberOfGenreItems_5;	
+
+}
