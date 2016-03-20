64840da13978eada7b2558c3c36f8be9a5613e41
DoctorD1501 <DoctorD1501@gmail.com>
2016-03-20 15:49:09 -0700

    Added "Preview Link in Browser" in manual search results dialog.

--- JAVMovieScraper/src/moviescraper/doctord/view/SelectionDialog.java
+++ JAVMovieScraper/src/moviescraper/doctord/view/SelectionDialog.java
@@ -1,12 +1,19 @@
 package moviescraper.doctord.view;
 
 import java.awt.BorderLayout;
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -28,6 +35,7 @@ public class SelectionDialog extends JPanel {
 	
 	private JList<SearchResult> labelList;
 	private SearchResult optionPickedFromPanel;
+	private JButton previewLinkButton;
 
 	/**
 	 * Create the frame.
@@ -39,6 +47,29 @@ public class SelectionDialog extends JPanel {
 		labelList.setVisible(true);
 		JScrollPane pane = new JScrollPane(labelList);
 		panel.add(pane, BorderLayout.CENTER);
+		
+		previewLinkButton = new JButton("Preview Link in Browser");
+		//previewLinkButton.setEnabled(false);
+		previewLinkButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SearchResult selectedValueinAction = labelList.getSelectedValue();
+				if(selectedValueinAction != null && selectedValueinAction.getUrlPath() != null && selectedValueinAction.getUrlPath().length() > 0) {
+					try {
+						Desktop.getDesktop().browse(new URI(selectedValueinAction.getUrlPath()));
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (URISyntaxException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				
+			}
+		});
+		panel.add(previewLinkButton, BorderLayout.SOUTH);
 		panel.setPreferredSize(new Dimension(500,400));
 		
 		final JDialog bwin = new JDialog();
